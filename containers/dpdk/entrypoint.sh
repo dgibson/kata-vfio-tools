621329126d8cf275ec150656b3762d9a6c927a5b
David Gibson <david@gibson.dropbear.id.au>
2020-08-04 09:13:04 +1000

    dpdk: Assorted fixes to dpdk container script
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- containers/dpdk/entrypoint.sh
+++ containers/dpdk/entrypoint.sh
@@ -10,40 +10,46 @@ echo "VFIO dpdk test container starting"
 ls -l /dev/vfio
 lspci -D
 
-set -e
+(
+    set -e
 
-if [ ! -c /dev/vfio/vfio ]; then
-    die "Container doesn't see VFIO control device"
-fi
+    if [ ! -c /dev/vfio/vfio ]; then
+	die "Container doesn't see VFIO control device"
+    fi
 
-VFIOGROUPS="$(cd /dev/vfio && echo [0-9]*)"
-NGROUPS=$(ls /dev/vfio | grep -v vfio | wc -l)
-echo "Container sees $NGROUPS IOMMU groups [$VFIOGROUPS]"
+    VFIOGROUPS="$(cd /dev/vfio && echo [0-9]*)"
+    NGROUPS=$(ls /dev/vfio | grep -v vfio | wc -l)
+    echo "Container sees $NGROUPS IOMMU groups [$VFIOGROUPS]"
 
-# Mount Hugepages. FIXME: This should be done by kata-agent
-mkdir -p /dev/hugepages; mount -t hugetlbfs nodev /dev/hugepages
+    # Mount Hugepages. FIXME: This should be done by kata-agent
+    #mkdir -p /dev/hugepages; mount -t hugetlbfs nodev /dev/hugepages
 
-CMD="testpmd -l0,1 --log-level=lib.eal:8"
+    CMD="testpmd -l0,1 --log-level=lib.eal:8"
 
-group=$(echo $VFIOGROUPS | cut -f1 -d' ')
-echo "Using group [$group]"
-devices=$(cd /sys/kernel/iommu_groups/$group/devices && echo *)
+    group=$(echo $VFIOGROUPS | cut -f1 -d' ')
+    echo "Using group [$group]"
+    devices=$(cd /sys/kernel/iommu_groups/$group/devices && echo *)
 
-echo "Using group $group ($devices)"
+    echo "Using group $group ($devices)"
 
-for dev in "$devices"; do
-    CMD="$CMD -w$dev"
-done
+    for dev in "$devices"; do
+	    # Ignore bridges
+	    if [ -d /sys/bus/pci/devices/$dev/pci_bus ]; then
+		continue
+	    fi
+	CMD="$CMD -w$dev"
+    done
 
-CMD="$CMD -- --stats-period 2 --forward-mode=txonly -a"
+    CMD="$CMD -- --stats-period 2 --forward-mode=txonly -a"
 
-# RUN Debug commands
-for dev in "$devices"; do
-    lspci -vv -s $dev
-done
+    # RUN Debug commands
+    for dev in "$devices"; do
+	lspci -vv -s $dev
+    done
 
-echo "About to run: $CMD"
+    echo "About to run: $CMD"
 
-$CMD
+    $CMD
+)
 
 exec /bin/bash
