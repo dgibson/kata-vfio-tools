fab1c7b317c44cb03e6a20c75d37309f92cff66f
David Gibson <david@gibson.dropbear.id.au>
2022-01-20 17:38:53 +1100

    First attempt at a Kubernetes usable testpmd case

---
+++ containers/k8s-dpdk/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+die() {
+    echo FAILED "$@" >&2
+    exit 1
+}
+
+echo "k8s VFIO dpdk test container"
+
+ls -l /dev/vfio
+env | grep '^PCIDEVICE'
+
+(
+    set -e
+
+    if [ ! -c /dev/vfio/vfio ]; then
+	die "Container doesn't see VFIO control device"
+    fi
+
+    CMD="dpdk-testpmd -l0,1 --log-level=lib.eal:8"
+
+    DEV="$PCIDEVICE_OPENSHIFT_IO_INTELNICS"
+    echo "Using device: $DEV"
+    echo "Forward mode is: $FORWARD_MODE"
+
+    lspci -D -v -s $DEV
+
+    CMD="$CMD -a $DEV"
+
+    CMD="$CMD -- --stats-period=2 --forward-mode=$FORWARD_MODE -a"
+
+    echo "About to run: $CMD"
+
+    $CMD
+)
+
+exec sleep infinity
